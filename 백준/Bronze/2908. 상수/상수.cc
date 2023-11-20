6064d842663529756b6668c24d5b7e01214a63dc
dbehdgns1215 <77192122+dbehdgns1215@users.noreply.github.com>
2023-11-20 23:45:03 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	string s1, s2;
+
+	cin >> s1 >> s2;
+
+
+	reverse(s1.begin(), s1.end());
+	reverse(s2.begin(), s2.end());
+
+	if (s1 > s2)
+	{
+		cout << s1;
+	}
+	else cout << s2;
+
+}

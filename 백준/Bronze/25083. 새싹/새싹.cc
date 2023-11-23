1b252b4692c14076ddaa81c9acb2940c0d564064
dbehdgns1215 <77192122+dbehdgns1215@users.noreply.github.com>
2023-11-23 23:15:42 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cout << "         ,r'\"7" << endl;
+	cout << "r`-_   ,'  ,/" << endl;
+	cout << " \\. \". L_r'" << endl;
+	cout << "   `~\\/" << endl;
+	cout << "      |" << endl;
+	cout << "      |" << endl;
+}

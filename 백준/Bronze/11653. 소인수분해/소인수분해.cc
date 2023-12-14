f69592188f87e24c31f259a189f1f06096afc0cc
dbehdgns1215 <77192122+dbehdgns1215@users.noreply.github.com>
2023-12-14 23:30:12 +0900

    [Bronze I] Title: 소인수분해, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N;
+
+    cin >> N;
+
+    if (N == 1) { return 0; }
+    for (int i = 2; i <= N; i++) {
+        while (N % i == 0) {
+            cout << i << '\n';
+            N /= i;
+        }
+	}
+}

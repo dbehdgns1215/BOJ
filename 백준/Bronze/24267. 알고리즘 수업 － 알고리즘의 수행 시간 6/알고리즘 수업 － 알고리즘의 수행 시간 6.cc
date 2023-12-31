c4088202fff0869a4c66ad89d7242ececc6cb9a2
dbehdgns1215 <77192122+dbehdgns1215@users.noreply.github.com>
2023-12-31 23:44:12 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	long long N;
+	
+	cin >> N;
+
+	cout << (N - 2) * (N - 1) * N / 6 << "\n3";
+}

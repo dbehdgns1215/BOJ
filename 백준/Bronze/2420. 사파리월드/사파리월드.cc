02ec2e11ad22907debec55c10f939c008597b6c1
dbehdgns1215 <77192122+dbehdgns1215@users.noreply.github.com>
2024-03-07 23:49:47 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	long long a, b;
+
+	cin >> a >> b;
+	if (a > b)
+	{
+		cout << a - b;
+	}
+	else
+	{
+		cout << b - a;
+	}
+}

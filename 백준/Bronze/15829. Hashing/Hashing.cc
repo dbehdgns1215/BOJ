e6ecff21b4e11f653ca2f833e646c40b8f6faba3
dbehdgns1215 <77192122+dbehdgns1215@users.noreply.github.com>
2024-04-01 01:24:08 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -3,8 +3,17 @@ using namespace std;
 
 int L;
 string s;
-long long r = 1;
-long long ans;
+
+// long long -> 64비트 -(2^63)) ~ (2^63 - 1) ; -9,223,372,036,854,775,808 ~ 9,223,372,036,854,775,807
+// 부호비트로 1비트를 사용하기에 63승까지
+
+// unsigned long long -> 64비트 0 ~ (2^64 - 1) ; 대략 18,446,744,073,709,551,615
+// 분배법칙 = (A + B) mod M = A mod M + B mod M
+// 덧셈, 뺄셈, 곱셈에 적용 됨.
+
+unsigned long long r = 1;
+unsigned long long ans;
+static unsigned long long m = 1234567891;
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -15,8 +24,8 @@ int main() {
 
 	for (int i = 0; i < L; i++)
 	{
-		ans += (s[i] - 96) * r;
-		r *= 31;
+		ans = (ans + (s[i] - 'a' + 1) * r) % m; // 문자열의 각 문자에 대해 계산하고, 바로 모듈로 연산을 수행합니다.
+		r = (r * 31) % m; // r을 갱신할 때도 모듈로 연산을 수행하여 오버플로우를 방지합니다.
 	}
-	cout << ans % 1234567891;
+	cout << ans % m;
 }

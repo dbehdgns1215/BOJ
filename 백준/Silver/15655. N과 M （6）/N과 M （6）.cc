1bf1b54801f0a9adee482588af1d36635b6514a1
dbehdgns1215 <77192122+dbehdgns1215@users.noreply.github.com>
2024-07-31 23:09:15 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[8];
+bool isUsed[8];
+vector<int> v;
+
+void func(int k, int start) {
+    if (k == m) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = start; i < n; i++) {
+        if (!isUsed[i]) {
+            arr[k] = v[i];
+            isUsed[i] = true;
+            func(k + 1, i + 1);
+            isUsed[i] = false;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+    }
+
+    sort(v.begin(), v.end());
+
+    func(0, 0);
+}

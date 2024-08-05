fc0ccdda5eae0ec477b3b0f439d5d54642f81f15
dbehdgns1215 <77192122+dbehdgns1215@users.noreply.github.com>
2024-08-06 01:15:49 +0900

    [Silver II] Title: N과 M (11), Time: 116 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> nums;
+vector<int> sequence;
+
+void func(int k) {
+	if (k == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << sequence[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	int last = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (nums[i] != last)
+		{
+			sequence.push_back(nums[i]);
+			func(k + 1);
+			sequence.pop_back();
+			last = nums[i];
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		nums.push_back(temp);
+	}
+
+	sort(nums.begin(), nums.end());
+
+	func(0);
+
+}

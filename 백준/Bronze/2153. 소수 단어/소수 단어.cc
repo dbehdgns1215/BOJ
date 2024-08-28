62ad22c9310c0817eb5bc79f08090dec19d40ce1
dbehdgns1215 <77192122+dbehdgns1215@users.noreply.github.com>
2024-08-28 23:00:35 +0900

    [Bronze II] Title: 소수 단어, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Bronze/2153. 소수 단어/소수 단어.cc
+++ 백준/Bronze/2153. 소수 단어/소수 단어.cc
@@ -4,25 +4,6 @@ using namespace std;
 string word;
 int sum;
 
-void isPrime(int n) {
-	bool* isPrime = new bool[n + 1];// n까지 구해야하므로, n+1개를 동적할당
-
-	//먼저 모든 배열을 true로 초기화
-	for (int i = 0; i <= n; i++) {
-		isPrime[i] = true;
-	}
-
-
-	for (int i = 2; i <= n; i++) {
-		if (isPrime[i]) {//해당수가 소수라면, 출력하고 해당수를 제외한 배수들을 모두 제외
-			cout << i << " ";
-			for (int j = i * 2; j <= n; j += i) {
-				isPrime[j] = false;
-			}
-		}
-	}
-}
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);

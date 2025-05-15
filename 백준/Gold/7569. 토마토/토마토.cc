fd25b97ff14b0aea029a059556ddeaca082b0a50
Dongni <77192122+dbehdgns1215@users.noreply.github.com>
2025-05-15 17:29:40 +0900

    [Gold V] Title: 토마토, Time: 92 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -4,11 +4,12 @@ using namespace std;
 int h, n, m;
 int tomato[101][101][101];
 int dist[101][101][101];
-bool vis[101][101][101];
 
 int dx[6] = { 1, 0, -1, 0, 0, 0 };
 int dy[6] = { 0, 1, 0, -1, 0, 0 };
 int dz[6] = { 0, 0, 0, 0, 1, -1 };
+
+queue<tuple<int, int, int>> q;
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -16,70 +17,52 @@ int main() {
 
 	cin >> m >> n >> h;
 
-	queue<tuple<int, int, int>> Q;
-
-	for (int i = 0; i < h; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			for (int k = 0; k < m; k++) 
-			{
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < m; k++) {
 				cin >> tomato[i][j][k];
-				if (tomato[i][j][k] == 1)
-				{
-					Q.push({ i, j, k });
-					vis[i][j][k] = 1;
+
+				if (tomato[i][j][k] == 1) { // tomato[h][n][m] tomato[dz][dx][dy]
 					dist[i][j][k] = 0;
+					q.push(tie(i, j, k));
 				}
-				if (tomato[i][j][k] == 0)
-				{
+				if (tomato[i][j][k] == 0) {
 					dist[i][j][k] = -1;
 				}
 			}
 		}
 	}
 
-	if (Q.empty()) {
-		cout << -1;
-		return 0;
-	}
-
-	while (!Q.empty())
-	{
+	while (!q.empty()) {
 		int x, y, z;
-		tie(z, x, y) = Q.front(); Q.pop();
+		tie(z, x, y) = q.front(); q.pop();
 
 		for (int dir = 0; dir < 6; dir++) {
-			int nx = x + dx[dir];
-			int ny = y + dy[dir];
-			int nz = z + dz[dir];
+			int nx = dx[dir] + x;
+			int ny = dy[dir] + y;
+			int nz = dz[dir] + z;
 
-			if (nz < 0 || nz >= h || nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			if (vis[nz][nx][ny] || tomato[nz][nx][ny] == -1) continue;
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= h) continue;
+			if (dist[nz][nx][ny] >= 0) continue;
 
 			dist[nz][nx][ny] = dist[z][x][y] + 1;
-			vis[nz][nx][ny] = 1;
-			Q.push({ nz, nx, ny });
+			q.push(tie(nz, nx, ny));
 		}
 	}
 
-	int ans = 0;
-	
-	for (int i = 0; i < h; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			for (int k = 0; k < m; k++)
-			{
-				if (dist[i][j][k] == -1)
-				{
+	int days = 0;
+
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < m; k++) {
+				if (dist[i][j][k] == -1) {
 					cout << -1;
 					return 0;
 				}
-				ans = max(ans, dist[i][j][k]);
+				days = max(days, dist[i][j][k]);
 			}
 		}
 	}
-	cout << ans;
-	
+
+	cout << days;
 }

0d7bb15dbaefeea709a6ee4c97a27512bea2d4bc
Dongni <77192122+dbehdgns1215@users.noreply.github.com>
2025-05-17 01:02:36 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int T;
+int cnt;
+
+int recursion(string& s, int l, int r) {
+    cnt++;
+    if (l >= r) return 1;
+    else if (s[l] != s[r]) return 0;
+    else return recursion(s, l + 1, r - 1);
+}
+
+int isPalindrome(string& s) {
+    return recursion(s, 0, s.length() - 1);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        string s;
+        cin >> s;
+        cout << isPalindrome(s) << " ";
+        cout << cnt << "\n";
+        cnt = 0;
+    }
+}

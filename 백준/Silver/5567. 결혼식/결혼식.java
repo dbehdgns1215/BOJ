cc5a32af91b14bab1a859ae28f2e5dea3a4f8485
Dongni <77192122+dbehdgns1215@users.noreply.github.com>
2025-08-07 00:30:13 +0900

    [Silver II] Title: 결혼식, Time: 160 ms, Memory: 18872 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.java
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 public class Main {
 
     static boolean[][] network;
-    static boolean[] invited;
+    static boolean[] visited;
     static int[] depth;
     
     public static void main(String[] args) throws IOException {
@@ -18,24 +18,22 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
         
-        network = new boolean[N + 1][N + 1]; // A의 친구 B -> [A][B]
-        invited = new boolean[N + 1];
+        network = new boolean[N + 1][N + 1];
+        visited = new boolean[N + 1];
         depth = new int[N + 1];
-        
-        Queue<Integer> q = new LinkedList<>();
 
         for (int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            
             network[a][b] = true;
             network[b][a] = true;
         }
 
+        Queue<Integer> q = new LinkedList<>();
+        
         q.add(1);
-        invited[1] = true;
+        visited[1] = true;
 
         int inviteCnt = 0;
 
@@ -43,11 +41,11 @@ public class Main {
             int cur = q.poll();
 
             for (int i = 1; i <= N; i++) {
-                if (!invited[i] && network[cur][i]) {
+                if (!visited[i] && network[cur][i]) {
                     depth[i] = depth[cur] + 1;
                     if (depth[i] <= 2) {
                         inviteCnt++;
-                        invited[i] = true;
+                        visited[i] = true;
                         q.add(i);
                     }
                 }

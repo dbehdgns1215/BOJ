609216e7cddcce2f798d123eca84859d2d8f0338
Dongni <77192122+dbehdgns1215@users.noreply.github.com>
2026-04-10 14:00:47 +0900

    [Silver IV] Title: ATM, Time: 76 ms, Memory: 11944 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			pq.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		int sum = 0;
+		int prev = 0;
+		
+		for (int i = 0; i < N; i++) {
+			int cur = pq.poll();
+			prev += cur;
+			sum += prev;
+		}
+		
+		System.out.print(sum);
+	}
+
+}
